6724c6b42161be3325151b565b842452e35e7588
AC2-K <117526989+AC2-K@users.noreply.github.com>
2023-01-06 16:00:43 +0900

    s

---
+++ sub.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+//cout << fixed << setprecision(10);
+#define rep(i, N)  for(int i=0;i<(N);i++)
+#define all(x) (x).begin(),(x).end()
+#define popcount(x) __builtin_popcount(x)
+using ll = long long;
+using ld = long double;
+using graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int inf = 1e9;
+const ll infl = 1e18;
+const ld eps = ld(0.000000001);
+const long double pi = acos(-1);
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const int dx[4] = { 1,0,-1,0 };
+const int dy[4] = { 0,1,0,-1 };
+/*
+template<class T>using v=vector<T>;
+template<class T>using vv=v<v<T>>;
+template<class T>using vvv=v<vv<T>>;
+template<class T>using vvvv=v<vvv<T>>;
+*/
+template<class T>void chmax(T&x,T y){if(x<y)x=y;}
+template<class T>void chmin(T&x,T y){if(x>y)x=y;}
+//f:S->Sに対して、f^t(s)=gなる最小の非負整数tを求める
+template<class S,class T>class BSGS{
+    const function<S(S)> f;
+    const function<T<S>> inv;   //逆写像
+    map<S,int> mp;
+    int m;
+    int n;
+public:
+    BSGS(int n,function<T(S)> f,function<T(S)> inv){
+        
+    }
+    void init(){
+
+    }
+};
+int main() {
+
+}
