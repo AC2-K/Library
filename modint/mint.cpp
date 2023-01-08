e72e81ca2b6ff70ee3be3f9aaa7a50e8c5bfdef0
AC2-K <117526989+AC2-K@users.noreply.github.com>
2023-01-08 21:50:53 +0900

    Update mint.cpp

--- modint/mint.cpp
+++ modint/mint.cpp
@@ -1,81 +1,93 @@
-
 template<ll mod>class modint {
 private:
-    using mint = modint<mod>;
-    ll value = 0;
+	using mint = modint<mod>;
+	ll value = 0;
 public:
+	modint(ll v = 0) {
+		v %= mod;
+		v = (v + mod) % mod;
+		value = v;
+	}
+	ll val() {
+		return value;
+	}
+	mint operator+(mint fp) {
+		return mint(value + fp.val());
+	}
+	mint operator-(mint fp) {
+		return mint(value - fp.val());
+	}
+	mint operator*(mint fp) {
+		return mint(value * fp.val());
+	}
+	void operator=(mint fp) {
+		val = fp.val();
+	}
+	void operator=(ll val) {
+		value = val;
+	}
+	void operator+=(mint fp) {
+		value = ((value + fp.val() + mod) % mod + mod) % mod;
+	}
+	void operator-=(mint fp) {
+		value = value - fp.val();
+		value %= mod;
+		value += mod;
+		value %= mod;
+	}
+	void operator*=(mint fp) {
+		value = value * fp.val();
+		value = (value % mod + mod) % mod;
+	}
+	bool operator==(mint fp) {
+		return value == fp.val();
+	}
+	bool operator<(mint fp) {
+		return value < fp.val();
+	}
+	bool operator>(mint fp) {
+		return value > fp.val();
+	}
+	bool operator<=(mint fp) {
+		return value <= fp.val();
+	}
+	bool operator>=(mint fp) {
+		return value >= fp.val();
+	}
 
-    modint(ll v = 0) {
-        v %= mod;
-        v = (v + mod) % mod;
-        value = v;
-    }
-    ll val() {
-        return value;
-    }
-    mint operator+(mint fp) {
-        return mint(value + fp.val());
-    }
-    mint operator-(mint fp) {
-        return mint(value - fp.val());
-    }
-    mint operator*(mint fp) {
-        return mint(value * fp.val());
-    }
-
-    void operator+=(mint fp) {
-        value = ((value + fp.val() + mod) % mod + mod) % mod;
-    }
-    void operator-=(mint fp) {
-        value = value - fp.val();
-        value %= mod;
-        value += mod;
-        value %= mod;
-    }
-    void operator*=(mint fp) {
-        value = value * fp.val();
-        value = (value % mod + mod) % mod;
-    }
-    bool operator==(mint fp) {
-        return value == fp.val();
-    }
-    bool operator<(mint fp) {
-        return value < fp.val();
-    }
-    bool operator>(mint fp) {
-        return value > fp.val();
-    }
-    bool operator<=(mint fp) {
-        return value <= fp.val();
-    }
-    bool operator>=(mint fp) {
-        return value >= fp.val();
-    }
-
-    ll inv() {
-        return mod_pow(mod - 2);
-    }
-    ll mod_pow(ll exp) {
-        if (exp < 0)exp += (mod - 1);
-        if (exp == 0)return 1;
-        if (exp == 1)return value;
-        ll ret = mod_pow(exp / 2);
-        return ret * ret % mod * mod_pow(exp % 2)%mod;
-    }
-    mint operator/(mint a) {
-        return mint(value * a.inv());
-    }
-    void operator/=(mint a) {
-        value = value * a.inv();
-        value%=mod;
-    }
+	mint inv() {
+		return mod_pow(mod - 2);
+	}
+	mint mod_pow(ll exp) {
+		ll base = value;
+		ll ans = 1;
+		base %= mod;
+		while (exp > 0) {
+			if (exp & 1) {
+				ans *= base;
+				ans %= mod;
+			}
+			base *= base;
+			base %= mod;
+			exp >>= 1;
+		}
+		return ans;
+	}
+	mint operator/(mint a) {
+		return mint(a.inv()*value);
+	}
+	void operator/=(mint a) {
+		value = value * a.inv();
+		value %= mod;
+	}
+	//入出力
 };
 template<ll T>istream& operator>>(istream& is, modint<T>& mt) {
-    ll v;
-    is >> v;
-    mt = v;
-    return is;
+	ll v;
+	is >> v;
+	mt = v;
+	return is;
 }
 template<ll T>ostream& operator << (ostream& os, modint<T>& mt) {
-    return os << mt.val()%T;
+	return os << mt.val() % T;
 }
