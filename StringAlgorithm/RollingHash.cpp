2ed0ee09fe901198a8230351bae5d612506e76c6
AC2-K <117526989+AC2-K@users.noreply.github.com>
2023-01-12 18:33:58 +0900

    UpdateRollingHash

--- StringAlgorithm/RollingHash.cpp
+++ StringAlgorithm/RollingHash.cpp
@@ -1,40 +1,38 @@
 #include"template.hpp"
-
 class RollingHash {
-	
 	static const ll mod = 1e9 + 7;
 	const ll base;
-	vector<ll> powers;	//base^i
+	vector<ll> pow;
 	static inline ll generate_base() {
 		mt19937_64 engine(chrono::steady_clock::now().time_since_epoch().count());
 		uniform_int_distribution<ll> rand((ll)1, (ll)mod - 1);
 		return rand(engine);
 	}
-	//idの振り方
-	ll mapping(char c) {
-		return (c - 'a');
+	//id
+	ll id(char c) {
+		return (c - 'a' + 1);
 	}
 	void expand(int siz) {
-		if (powers.size() < siz + 1) {
-			int pre_siz = powers.size();
-			powers.resize(siz + 1);
-			for (int i = pre_siz; i <= siz; i++) {
-				powers[i] = (powers[i - 1] * base) % mod;
-			}
+		if ((int)pow.size() >= siz)return;
+
+		int pre_siz = pow.size();
+		pow.resize(siz + 1);
+		for (int i = pre_siz; i <= siz; i++) {
+			pow[i] = (pow[i - 1] * base) % mod;
 		}
 	}
 
 public:
-	RollingHash(ll base = generate_base()) :base(generate_base()), powers{ 1 } {	} 
+	RollingHash(ll base_ = generate_base()) :base(base_), pow{ 1 } {	}
 	vector<ll> build(string& s) {
 		vector<ll> hash(s.size() + 1);
-		for (int i = 0; i < s.size(); i++) {
-			hash[i + 1] = (hash[i] * base % mod + mapping(s[i])) % mod;
+		for (int i = 0; i < (int)s.size(); i++) {
+			hash[i + 1] = (hash[i] * base % mod + id(s[i])) % mod;
 		}
 		return hash;
 	}
-	ll range(vector<ll>&hash,int l, int r) {
+	ll range(vector<ll>& hash, int l, int r) {
 		expand(r - l);
-		return ((hash.at(r) + mod - hash.at(l) * powers[r - l]) % mod + mod) % mod;
+		return ((hash[r] + mod - hash[l] * pow[r - l]) % mod + mod) % mod;
 	}
 };
--- main.cpp
+++ main.cpp
@@ -18,23 +18,43 @@ const int dx[4] = { 1,0,-1,0 };
 const int dy[4] = { 0,1,0,-1 };
 template<class T>void chmax(T&x,T y){if(x<y)x=y;}
 template<class T>void chmin(T&x,T y){if(x>y)x=y;}
-vector<int> press(vector<int> A){
-    vector<int> B;
-    vector<int> T;
-    rep(i,A.size())T.push_back(A[i]);
-    sort(all(T));
-    T.erase(unique(all(T)),T.end());
-    rep(i,A.size()){
-        int pos=lower_bound(all(T),A[i])-T.begin();
-        B.push_back(pos);
+class RollingHash {
+    static const ll mod = 1e9 + 7;
+    const ll base;
+    vector<ll> powers;	//base^i
+    static inline ll generate_base() {
+        mt19937_64 engine(chrono::steady_clock::now().time_since_epoch().count());
+        uniform_int_distribution<ll> rand((ll)1, (ll)mod - 1);
+        return rand(engine);
     }
-    return B;
-}
+    //idの振り方
+    ll id(char c) {
+        return (c - 'a');
+    }
+    void expand(int siz) {
+        if (powers.size() < siz + 1) {
+            int pre_siz = powers.size();
+            powers.resize(siz + 1);
+            for (int i = pre_siz; i <= siz; i++) {
+                powers[i] = (powers[i - 1] * base) % mod;
+            }
+        }
+    }
+
+public:
+    RollingHash(ll base = generate_base()) :base(generate_base()), powers{ 1 } {	} 
+    vector<ll> build(string& s) {
+        vector<ll> hash(s.size() + 1);
+        for (int i = 0; i < s.size(); i++) {
+            hash[i + 1] = (hash[i] * base % mod + mapping(s[i])) % mod;
+        }
+        return hash;
+    }
+    ll range(vector<ll>&hash,int l, int r) {
+        expand(r - l);
+        return ((hash[r] + mod - hash[l] * powers[r - l]) % mod + mod) % mod;
+    }
+};
 int main() {
-    int n;
-    cin>>n;
-    vector<int> a(n);
-    for(auto&aa:a)cin>>aa;
-    a=press(a);
-    for(auto&aa:a)cout<<aa<<endl;
+
 }
