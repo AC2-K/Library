aee3bb046dea6e7b80de5e855ab66b9098e44c9a
AC2K <117526989+AC2-K@users.noreply.github.com>
2023-01-18 11:08:14 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,61 +1,67 @@
+#define Problem https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_A
 #include<bits/stdc++.h>
 using namespace std;
-//cout << fixed << setprecision(10);
 #define rep(i, N)  for(int i=0;i<(N);i++)
 #define all(x) (x).begin(),(x).end()
 #define popcount(x) __builtin_popcount(x)
 using ll = long long;
+//using i128=__int128_t;
 using ld = long double;
 using graph = vector<vector<int>>;
 using P = pair<int, int>;
 const int inf = 1e9;
 const ll infl = 1e18;
-const ld eps = ld(0.000000001);
+const ld eps = 1e-6;
 const long double pi = acos(-1);
-//const ll MOD = 1e9 + 7;
+const ll MOD = 1e9 + 7;
 const ll MOD2 = 998244353;
 const int dx[4] = { 1,0,-1,0 };
 const int dy[4] = { 0,1,0,-1 };
-/*
-template<class T>using v=vector<T>;
-template<class T>using vv=v<v<T>>;
-template<class T>using vvv=v<vv<T>>;
-template<class T>using vvvv=v<vvv<T>>;
-*/
 template<class T>void chmax(T&x,T y){if(x<y)x=y;}
 template<class T>void chmin(T&x,T y){if(x>y)x=y;}
-
-ll modlog(ll x, ll y,const int&MOD) {
-    if(x==0){
-        if(y==0)return 0;
-        else return -1;
+vector<ll> dijkstra(int s,const vector<vector<pair<ll,ll>>>&g){
+    int n=g.size();
+    priority_queue<P,vector<P>,greater<P>> que;
+    que.emplace(0,s);
+    vector<ll> dist(n,infl);
+    vector<bool> confirm(n,false);
+    dist[s]=0;
+    while(!que.empty()){
+        auto T=que.top();
+        que.pop();
+        int vc=T.first,vv=T.second;
+        if(confirm[vv])continue;
+        confirm[vv]=true;
+        for(auto nex:g[vv]){
+            int nv=nex.first,nc=nex.second;
+            if(dist[nv]<dist[vv]+nc)continue;
+            dist[nv]=dist[vv]+nc;
+            que.emplace(dist[nv],nv);
+        }
     }
-    ll H = sqrt(MOD) + 1;
-    static pair<ll, ll> baby[100010];
-    for (ll b = 0, xby = y; b < H; b++, xby = (xby * x) % MOD) {
-        baby[b] = std::make_pair(xby, b);
-    }
-    sort(baby, baby + H);
-    ll xH = 1;
-    for (int i = 0; i < H; ++i) {
-        xH = (xH * x) % MOD;
-    }
-    for (ll a = 1, xaH = xH; a <= H; a++, xaH = (xaH * xH) % MOD) {
-        auto it = std::lower_bound(baby, baby + H, std::make_pair(xaH + 1, 0LL));
-        if (it == baby) continue;
-        it--;
-        if (it->first == xaH) return a * H - it->second;
-    }
-    return -1;
+    return dist;
 }
 int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        ll x,y,p;
-        cin>>x>>y>>p;
-        x%=p;
-        y%=p;
-        cout<<modlog(x,y,p)<<endl;
+    int n;
+    cin>>n;
+    vector<vector<pair<ll,ll>>> g(n);
+    rep(i,n-1){
+        int s,t,w;
+        cin>>s>>t>>w;
+        g[s].emplace_back(t,w);
+        g[t].emplace_back(s,w);
     }
+
+    auto dist=dijkstra(0,g);
+    int v=0;
+    ll now_max=0;
+    rep(r,n){
+        if(dist[r]>now_max){
+            v=r;
+            now_max=dist[r];
+        }
+    }
+
+    dist=dijkstra(v,g);
+    cout<<(*max_element(all(dist)))<<endl;
 }
