f816f36f6be4194a5c8c8008983af0223efa4333
AC2K <yokouchiko@gmail.com>
2023-02-16 23:35:13 +0900

    update BabyStepGiantStep

--- Math/BabyStepGiantStep.cpp
+++ Math/BabyStepGiantStep.cpp
@@ -1,20 +1,19 @@
 ll bsgs(S s,S g,ll n,ll m,const function<S(S)>&f,const function<S(S)>&fm){
-    map<S,int> mp;
+    map<S,ll> mp;
     for(int j=1;j<=m;j++){
         g=f(g);
-        mp[g]=-j;
+        mp[g]=j;
     }
     for(int i=1;;i++){
         s=fm(s);
 
-        if(mp.count(s)){
-            return m*i+mp[s];
+        if(mp.find(s)!=mp.end()){
+            return m*i-mp[s];
         }
 
         if(n<=m*i)break;
     }
-
     return -1;
 }
 /// @brief BabyStep GiantStep 
-///全単射f:S->Sについて、f^m(s)=gなる最小のmを求める
+//条件を満たすf:S->Sについて、f^m(s)=gなる最小のmを求める
