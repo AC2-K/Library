27ac6a2e3f9ebbe09aadb87ee5f53ffcf37d989d
AC2K <yokouchiko@gmail.com>
2023-02-21 13:55:41 +0900

    master

--- main.cpp
+++ main.cpp
@@ -1,36 +1,23 @@
-#include"template.hpp"
-#include"data-structure/lazy_segtree.hpp"
-
-int op(int x,int y){return x+y;}
-int comp(int x,int y){return x+y;}
-int mapping(int x,int y){return x+y;}
-int e(){return 0;}
-int id(){return 0;}
-
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, N)  for(int i=0;i<(N);i++)
+#define all(x) (x).begin(),(x).end()
+#define popcount(x) __builtin_popcount(x)
+using ll = long long;
+//using i128=__int128_t;
+using ld = long double;
+using graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int inf = 1e9;
+const ll infl = 1e18;
+const ld eps = 1e-6;
+const long double pi = acos(-1);
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const int dx[4] = { 1,0,-1,0 };
+const int dy[4] = { 0,1,0,-1 };
+template<class T>inline void chmax(T&x,T y){if(x<y)x=y;}
+template<class T>inline void chmin(T&x,T y){if(x>y)x=y;}
 int main() {
-    int n,q;
-    cin>>n>>q;
-    lazy_segtree<int,int,op,e,comp,id,mapping> seg(n);
-    while(q--){
-        int t;
-        cin>>t;
-        if(t==0){
-            int l,r;
-            cin>>l>>r;
-            int new_val;
-            cin>>new_val;
-            l--,r--;
-            seg.update(l,r+1,new_val);
-        }else{
-            int i;
-            cin>>i;
-            i--;
-            cout<<seg[i]<<'\n';
-        }
-        /*
-        cout<<"[";
-        rep(i,n)cout<<seg[i]<<',';
-        cout<<"]\n";
-        */
-    }
+    
 }
