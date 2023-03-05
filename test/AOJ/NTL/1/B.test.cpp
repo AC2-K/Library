61499e8f1df9326e1bcb6bd847d9cff970d28d8d
AC2K <yokouchiko@gmail.com>
2023-03-05 19:19:07 +0900

    test

--- test/AOJ/NTL/1/B.test.cpp
+++ test/AOJ/NTL/1/B.test.cpp
@@ -2,7 +2,7 @@
 #include "template.hpp"
 #include "math/mod_pow.hpp"
 int main(){
-    int n,m;
-    cin>>n>>m;
-    cout<<mod_pow<ll>(n,m,MOD)<<'\n';
+    int n, m;
+    cin >> n >> m;
+    cout << mod_pow<ll>(n, m, MOD) << '\n';
 }
