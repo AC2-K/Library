8329203a44f49e27141487d0bc632423ffcd2807
AC2K <yokouchiko@gmail.com>
2023-03-05 19:07:04 +0900

    test

--- test/AOJ/NTL/1/B.test.cpp
+++ test/AOJ/NTL/1/B.test.cpp
@@ -1,7 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/NTL_1_B"
 #include "template.hpp"
 #include "math/mod_pow.hpp"
-#line 3 "main.cpp"
 int main(){
     int n,m;
     cin>>n>>m;
