fa0692183adad7bb8af62c270346f4af8eaaa930
AC2K <yokouchiko@gmail.com>
2023-03-07 15:26:06 +0900

    update RollingHash

--- string/rolling_hash.hpp
+++ string/rolling_hash.hpp
@@ -1,56 +1,60 @@
 #pragma once
-#include"math/large_mod.hpp"
-namespace for_hash {
-	using ull = uint_fast64_t;
-	constexpr ull mapping_max = (ull)'Z';
-	ull base = -1;	//baseをランダムにとる
-	constexpr ull mod = (1uL << 61) - 1;
-	constexpr ull primitive_root = 37;
-	ull strong_rand() {
-		mt19937_64 engine(chrono::steady_clock::now().time_since_epoch().count());
-		uniform_int_distribution<ull> rand(1uL, mod - 1);
-		return rand(engine);
-	}
-	void generate_base() {
-        if(base != -1) return;
-		ull r = mod - 1;
-		while (gcd(r, mod - 1) != 1 || r <= mapping_max) {
-			r = strong_rand();
-		}
-		base = large_modpow(primitive_root, r, mod);
-	}
-};
-
+#include"math/mod_pow.hpp"
 class RollingHash {
 	using ull = uint_fast64_t;
-	using i128 = __int128_t;	//変えましょうね~
+    using i128 = __int128_t;
+    using u128 = __uint128_t;
+    // mod
 	static constexpr ull msk30 = (1ul << 30) - 1;
 	static constexpr ull msk61 = (1ul << 31) - 1;
 	string str;
 	vector<ull> hash, pow;
 
+    static const ull mod = (1uL << 61) - 1;
+    static const ull primitive_root = 37;
+public:
+	static const uint mapping_max = 'Z' + 1;
+	static ull base;
+private:
+	ull mul(const u128& a,const u128& b) const {
+        u128 t = a * b;
 
-	ull mul(i128 a, i128 b)const {
-		i128 t = a * b;
+		t = (t >> 61) + (t & mod);
 
-		t = (t >> 61) + (t & for_hash::mod);
-
-		if (t >= for_hash::mod) {
-			t -= for_hash::mod;
+		if (t >= mod) {
+			t -= mod;
 		}
 
 
 		return t;
+    }
+
+    ull mapping(const char& c) const {
+		return (ull)c;	//変更する?
 	}
 
-	ull mapping(const char& c) {
-		return (ull)c;	//変更する?
+
+    static ull strong_rand() {
+		mt19937_64 engine(chrono::steady_clock::now().time_since_epoch().count());
+		uniform_int_distribution<ull> rand(1uL, mod - 1);
+		return rand(engine);
+	}	
+    static void generate_base() {
+		if (base != 0){
+			return;
+		}
+		ull r = mod - 1;
+
+		while (gcd(r, mod - 1) != 1 || r <= mapping_max){
+			r = strong_rand();
+		}
+		base = mod_pow<__uint128_t>(primitive_root, r, mod);
 	}
 public:
 	RollingHash() :str() {	}
 
 	RollingHash(const string& str) :str(str) {
-		for_hash::generate_base();
+		generate_base();
 		build();
 	}
 
@@ -59,21 +63,38 @@ public:
 		pow.resize(str.size() + 1, 1);
 
 		for (int i = 0; i < str.size(); i++) {
-			hash[i + 1] = mul(hash[i], for_hash::base) + mapping(str[i]);
-			pow[i + 1] = mul(pow[i], for_hash::base);
-			if (hash[i + 1] >= for_hash::mod) {
-				hash[i + 1] -= for_hash::mod;
+			hash[i + 1] = mul(hash[i], base) + mapping(str[i]);
+			pow[i + 1] = mul(pow[i], base);
+			if (hash[i + 1] >= mod) {
+				hash[i + 1] -= mod;
 			}
 		}
 	}
 	ull range(int l, int r) const {
-		ull res = for_hash::mod + hash[r] - mul(hash[l], pow[r - l]);
-		return res < for_hash::mod ? res : res - for_hash::mod;
+		ull res = mod + hash[r] - mul(hash[l], pow[r - l]);
+		return res < mod ? res : res - mod;
 	}
-	ull get_all(){
+	ull get_all() const {
 		return hash.back();
 	}
-	int size(){return str.size();}
-};
+	int size() const {return str.size();}
 
-///@brief rolling hash
+    static int lcp(const RollingHash &a, const RollingHash &b, const int &start_a, const int &start_b) {
+        int ok = 0;
+        int ng = min(a.size() - start_a, b.size() - start_b) + 1;
+		while (abs(ok - ng) > 1){
+			int md = (ok + ng) >> 1;
+            if (a.range(start_a, start_a + md) == b.range(start_b, start_b + md)){
+                ok = md;
+            }
+            else{
+                ng = md;
+            }
+		}
+
+		return ok;
+    }
+};
+typename RollingHash::ull RollingHash::base;
+
+///@brief Rollinghash(ローリングハッシュ)
