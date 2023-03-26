7862e27fb72106d95cf4d633f73084c687fbefa6
AC2K <yokouchiko@gmail.com>
2023-03-26 18:34:28 +0900

    add verify

---
+++ test/yosupo judge/data structure/Static_Range_Frequency_Mo.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_frequency"
+#include"template.hpp"
+#include"other/mo.hpp"
+int main() {
+    int n, q;
+    scanf("%d%d", &n, &q);
+
+    vector<int> a(n);
+    for (auto& aa : a) {
+        scanf("%d", &aa);
+    }
+    auto pressed = a;
+    sort(pressed.begin(), pressed.end());
+    pressed.erase(unique(pressed.begin(), pressed.end()), pressed.end());
+    for (auto& ai : a) {
+        ai = lower_bound(pressed.begin(), pressed.end(), ai) - pressed.begin();
+    }
+    Mo mo(q);
+    vector<int> x(q);
+    for (int i = 0; i < q; i++) {
+        int l, r;
+        scanf("%d%d%d\n", &l, &r, &x[i]);
+        mo.add(l, r);
+    }
+
+    vector<int> cnt(pressed.size() + 1);
+    vector<int> ans(q);
+    auto add = [&](int v) -> void {
+        cnt[a[v]]++;
+    };
+    auto del = [&](int v) -> void {
+        cnt[a[v]]--;
+    };
+    auto out = [&](int v) -> void {
+        auto it = lower_bound(all(pressed), x[v]);
+        if (it == pressed.end() || (*it) != x[v]) {
+            ans[v] = 0;
+        } else {
+            int xi = it - pressed.begin();
+            ans[v] = cnt[xi];
+        }
+    };
+    mo.build(add, del, out);
+    for (auto& aa : ans) {
+        printf("%d\n", aa);
+    }
+}
