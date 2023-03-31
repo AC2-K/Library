7c7dc1f7ec1f4400edecaefb80e5918199ffc639
AC2K <yokouchiko@gmail.com>
2023-03-31 15:06:55 +0900

    feat

--- math/miller.hpp
+++ math/miller.hpp
@@ -1,57 +1,59 @@
 #pragma once
 #include"math/mod_pow.hpp"
 namespace prime {
-    namespace miller{
+    namespace miller {
         using i128 = __int128_t;
         using u128 = __uint128_t;
-        using u64 = __uint64_t;
-        constexpr bool miller_rabin(u64 n,const u64 bases[],int siz) {
+        using u64 = uint64_t;
+        using u32 = uint32_t;
+
+        bool miller_rabin(u64 n, const u64 bases[], int length) {
             u64 d = n - 1;
-            u64 q = __builtin_ctz(d);
-            d >>= q;
 
-            for (int i = 0; i < siz; i++) {
+            while (~d & 1) {
+                d >>= 1;
+            }
+
+            u64 rev = n - 1;
+
+            for (int i = 0; i < length; i++) {
                 u64 a = bases[i];
-                if (a == n) {
-                    return true;
-                } else if (n % a == 0) {
-                    return false;
-                }
-                if (mod_pow<u128>(a, d, n) != 1) {
-                    bool flag = true;
-                    for (u64 r = 0; r < q; r++) {
-                        u64 pow = mod_pow<u128>(a, d * (1ll << r), n);
-                        if (pow == n - 1) {
-                            flag = false;
-                            break;
-                        }
-                    }
 
-                    if (flag) {
-                        return false;
-                    }
+                if (n <= a) {
+                    return true;
                 }
+                u64 t = d;
+                u128 y = mod_pow<u128>(a, t, n);
+                while (t != n - 1 && y != 1 && y != rev) {
+                    (y *= y) %= n;
+                    t <<= 1;
+                }
+
+                if (y != rev && (~t & 1))return false;
             }
             return true;
         }
 
-        constexpr u64 bases_int[3] = {2, 7, 61};  // intだと、2,7,61で十分
-        constexpr u64 bases_ll[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
-        constexpr bool is_prime(u64 n){
-            
+
+        constexpr u64 bases_int[3] = { 2, 7, 61 };  // intだと、2,7,61で十分
+        constexpr u64 bases_ll[7] = { 2, 325, 9375, 28178, 450775, 9780504, 1795265022 };
+        bool is_prime(u64 n) {
             if (n < 2) {
                 return false;
-            } else if (n == 2) {
+            }
+            else if (n == 2) {
                 return true;
-            } else if (~n & 1) {
+            }
+            else if (~n & 1) {
                 return false;
             }
             if (n < (1ul << 31)) {
                 return miller_rabin(n, bases_int, 3);
-            } else {
+            }
+            else {
                 return miller_rabin(n, bases_ll, 7);
             }
         }
     };
 };
-///@brief fast prime check(MillerRabinの素数判定)
+///@brief MillerRabinの素数判定
