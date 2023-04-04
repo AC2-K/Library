187e2b1c1e2e1211dde171844dacef0c4f62e543
AC2K <yokouchiko@gmail.com>
2023-04-04 18:07:38 +0900

    test

---
+++ test/yosupo_judge/data_structure/Associative_Array_hashmap.test.cpp
@@ -0,0 +1,21 @@
+#include"template.hpp"
+#include"data-structure/hash_map.hpp"
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+int main() {
+    uint32_t q;
+    scanf("%d", &q);
+    hash_map<uint64_t, __uint64_t> mp;
+    while (q--) {
+        int t;
+        scanf("%d",&t);
+        if(!t){
+            __uint64_t k, v;
+            scanf("%lld%lld", &k, &v);
+            mp[k] = v;
+        }else{
+            __uint64_t k;
+            scanf("%lld", &k);
+            printf("%lld\n", mp[k]);
+        }
+    }
+}
