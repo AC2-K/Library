01c2a76c8162122c71bfe047a90701b5097d68f6
AC2-K <yokouchiko@gmail.com>
2023-04-09 12:37:26 +0900

    test Z

---
+++ test/yosupo_judge/string/Z_algorithm.hpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+#include"../../../src/string/Z.hpp"
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string s;
+    std::cin >> s;
+    auto res = kyopro::Z(s);
+    for(const auto&r:res){
+        std::cout << r << ' ';
+    }
+    std::cout << '\n';
+}
