09c39972768afecc991eda8bb917a18bc93a5185
AC2-K <yokouchiko@gmail.com>
2023-04-09 21:22:11 +0900

    created topological sort

---
+++ src/graph/topological_sort.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+
+namespace kyopro {
+void topological_sort(const std::vector<std::vector<int>>& g,
+                      std::vector<int>& res,
+                      bool& has_cycle) {
+    has_cycle = false;
+    int n = g.size();
+    std::vector<bool> vis(n, false), finished(n, false);
+
+    auto dfs = [&](auto f, int v) -> bool {
+        vis[v] = true;
+        for (const auto& nex : g[v]) {
+            if (vis[nex]) {
+                if (!finished[nex]) {
+                    return false;
+                }
+                continue;
+            }
+            if (!f(f, nex)) {
+                return false;
+            }
+        }
+
+        finished[v] = true;
+        res.push_back(v);
+
+        return true;
+    };
+    for (int v = 0; v < n; v++) {
+        if (vis[v]) continue;
+        if (!dfs(dfs, v)) {
+            has_cycle = true;
+            return;
+        }
+    }
+    std::reverse(res.begin(), res.end());
+}
+
+};  // namespace kyopro
