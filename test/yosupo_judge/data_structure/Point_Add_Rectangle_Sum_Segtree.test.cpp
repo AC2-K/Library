2be5b1f093f49be160da80ace8e72500749b3369
AC2-K <yokouchiko@gmail.com>
2023-04-23 13:17:06 +0900

    fix

--- test/yosupo_judge/data_structure/Point_Add_Rectangle_Sum_Segtree.test.cpp
+++ test/yosupo_judge/data_structure/Point_Add_Rectangle_Sum_Segtree.test.cpp
@@ -4,7 +4,6 @@
 inline long long op(long long a, long long b) { return a + b; };
 inline long long e() { return 0; }
 int main() {
-    using namespace std;
     kyopro::RangeTree<int, long long, op, e> rtree;
 
     int N, Q;
@@ -12,7 +11,7 @@ int main() {
     int X[N], Y[N], W[N];
     int c[Q];
     int s[Q], t[Q], u[Q], v[Q];
-    for (int i = 0; i < Q; ++i) {
+    for (int i = 0; i < N; ++i) {
         scanf("%d %d %d\n", &X[i], &Y[i], &W[i]);
         rtree.add_point(X[i], Y[i]);
     }
@@ -29,7 +28,7 @@ int main() {
     rtree.build();
 
     for (int i = 0; i < N; ++i) rtree.add(X[i], Y[i], W[i]);
-    
+
     for (int i = 0; i < Q; ++i) {
         if (c[i]) {
             printf("%lld\n", rtree.sum(s[i], t[i], u[i], v[i]));
--- test/yosupo_judge/data_structure/Static_Range_Sum.test.cpp
+++ test/yosupo_judge/data_structure/Static_Range_Sum.test.cpp
@@ -2,25 +2,25 @@
 #include <iostream>
 #include "../../../src/data-structure/segtree.hpp"
 
-namespace for_segtree{
-    using S = long long;
-    S op(S x,S y){return x+y;}
-    S e(){return 0;}
-};
-int main(){
-    int n,q;
-    scanf("%d%d",&n,&q);
+namespace for_segtree {
+using S = long long;
+S op(S x, S y) { return x + y; }
+S e() { return 0; }
+};  // namespace for_segtree
+int main() {
+    int n, q;
+    scanf("%d%d", &n, &q);
 
-    kyopro::segtree<for_segtree::S,for_segtree::op,for_segtree::e> seg(n);
-    for(int i=0;i<n;i++){
+    kyopro::segtree<for_segtree::S, for_segtree::op, for_segtree::e> seg(n);
+    for (int i = 0; i < n; i++) {
         int a;
-        scanf("%d",&a);
-        seg.set(i,a);
+        scanf("%d", &a);
+        seg.set(i, a);
     }
     seg.build();
-    while(q--){
-        int l,r;
-        scanf("%d%d",&l,&r);
-        printf("%lld\n",seg.prod(l,r));
+    while (q--) {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        printf("%lld\n", seg.prod(l, r));
     }
 }
