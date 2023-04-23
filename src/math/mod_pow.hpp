4229fd84c4eea8f21e88a11f3a9dede23e1b3445
AC2-K <yokouchiko@gmail.com>
2023-04-23 13:59:16 +0900

    fix

--- src/math/mod_pow.hpp
+++ src/math/mod_pow.hpp
@@ -3,7 +3,8 @@
 namespace kyopro {
 
 ///@brief mod pow(繰り返しニ乗法)
-template <class T> constexpr T mod_pow(T base, T exp, T mod) {
+template <typename T> 
+constexpr T mod_pow(internal::double_size_uint_t<T> base, T exp, T mod) {
     internal::double_size_uint_t<T> ans = (mod == 1 ? 0 : 1);
     base %= mod;
     while (exp) {
