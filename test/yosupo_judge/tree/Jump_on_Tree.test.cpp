24c28fac759f9d48e2f5f7097c4253cc3b38b92e
AC2-K <yokouchiko@gmail.com>
2023-05-14 13:15:08 +0900

    [test]Add test

---
+++ test/yosupo_judge/tree/Jump_on_Tree.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/jump_on_tree"
+#include "stream.hpp"
+#include "tree/doubling.hpp"
+int main() {
+    int n, q;
+    kyopro::readint(n, q);
+    kyopro::doubling_on_tree g(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b;
+        kyopro::readint(a, b);
+        g.add_edge(a, b);
+    }
+    g.build();
+
+    while (q--) {
+        int s, t, i;
+        kyopro::readint(s, t, i);
+        kyopro::putint(g.jump(s, t, i));
+    }
+}
