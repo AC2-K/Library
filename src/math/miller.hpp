182d0893a15b23196009aaa556a7c53fde1719d1
AC2K <117526989+AC2-K@users.noreply.github.com>
2023-05-15 05:34:08 +0000

    [Update]Add access modifier

--- src/math/miller.hpp
+++ src/math/miller.hpp
@@ -1,68 +1,67 @@
 #pragma once
 #include "../math/dynamic_modint.hpp"
 namespace kyopro {
-namespace miller {
-using i128 = __int128_t;
-using u128 = __uint128_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-
-using i128 = __int128_t;
-using u128 = __uint128_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-
-template <typename T, typename mint, const int bases[], int length>
-constexpr bool miller_rabin(T n) {
-    T d = n - 1;
-
-    while (~d & 1) {
-        d >>= 1;
-    }
-
-    const T rev = n - 1;
-    if (mint::get_mod() != n) {
-        mint::set_mod(n);
-    }
-    for (int i = 0; i < length; ++i) {
-        if (n <= bases[i]) {
-            return true;
-        }
-        T t = d;
-        mint y = mint(bases[i]).pow(t);
-        
-        while (t != n - 1 && y.val() != 1 && y.val() != rev) {
-            y *= y;
-            t <<= 1;
-        }
-
-        if (y.val() != rev && (~t & 1)) return false;
-    }
-    return true;
-}
-// 底
-constexpr int bases_int[3] = {2, 7, 61};
-constexpr int bases_ll[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 
 /**
  * @brief MillerRabin素数判定法
-*/
-template <typename T>
-constexpr bool inline is_prime(T n) {
-    if (n < 2) {
-        return false;
-    } else if (n == 2) {
+ */
+class miller {
+    using i128 = __int128_t;
+    using u128 = __uint128_t;
+    using u64 = uint64_t;
+    using u32 = uint32_t;
+
+    template <typename T, typename mint, const int bases[], int length>
+    static constexpr bool miller_rabin(T n) {
+        T d = n - 1;
+
+        while (~d & 1) {
+            d >>= 1;
+        }
+
+        const T rev = n - 1;
+        if (mint::get_mod() != n) {
+            mint::set_mod(n);
+        }
+        for (int i = 0; i < length; ++i) {
+            if (n <= bases[i]) {
+                return true;
+            }
+            T t = d;
+            mint y = mint(bases[i]).pow(t);
+
+            while (t != n - 1 && y.val() != 1 && y.val() != rev) {
+                y *= y;
+                t <<= 1;
+            }
+
+            if (y.val() != rev && (~t & 1)) return false;
+        }
         return true;
-    } else if (~n & 1) {
-        return false;
-    };
-    if (std::numeric_limits<T>::digits < 32 || n <= 1 << 30) {
-        return miller_rabin<T, dynamic_modint<std::make_unsigned_t<T>>, bases_int, 3>(n);
-    } else {
-        return miller_rabin<T, dynamic_modint<std::make_unsigned_t<T>>, bases_ll, 7>(n);
     }
-    return false;
-}
+    // 底
+    static constexpr int bases_int[3] = {2, 7, 61};
+    static constexpr int bases_ll[7] = {2,      325,     9375,      28178,
+                                        450775, 9780504, 1795265022};
+
+public:
+    template <typename T>
+    static constexpr bool inline is_prime(T n) {
+        if (n < 2) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (~n & 1) {
+            return false;
+        };
+        if (std::numeric_limits<T>::digits < 32 || n <= 1 << 30) {
+            return miller_rabin<T, dynamic_modint<std::make_unsigned_t<T>>,
+                                bases_int, 3>(n);
+        } else {
+            return miller_rabin<T, dynamic_modint<std::make_unsigned_t<T>>,
+                                bases_ll, 7>(n);
+        }
+        return false;
+    }
 };  // namespace miller
 };  // namespace kyopro
-
--- src/math/rho.hpp
+++ src/math/rho.hpp
@@ -8,95 +8,96 @@ namespace kyopro {
 
 /**
  * @brief Pollard Rho 素因数分解法
-*/
-namespace rho {
-using namespace std;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
+ */
+class rho {
+    using i128 = __int128_t;
+    using u128 = __uint128_t;
+    using u64 = uint64_t;
+    using u32 = uint32_t;
 
-template <typename mint> 
-u64 find_factor(u64 n) {
-    xor_shift32 rng(2023);
+    template <typename mint>
+    static u64 find_factor(u64 n) {
+        xor_shift32 rng(2023);
 
-    if (~n & 1uL) {
-        return 2;
-    }
-    if (kyopro::miller::is_prime(n)) {
-        return n;
-    }
-
-    if (mint::get_mod() != n) {
-        mint::set_mod(n);
-    }
-    while (1) {
-        u64 c = rng();
-        auto f = [&](mint x) -> mint { return x * x + c; };
-        mint x = rng();
-        mint y = f(x);
-        u64 d = 1;
-        while (d == 1) {
-            d = _gcd<long long>(abs((long long)x.val() - (long long)y.val()),
-                                n);
-            x = f(x);
-            y = f(f(y));
+        if (~n & 1uL) {
+            return 2;
         }
-        if (1 < d && d < n) {
-            return d;
+        if (kyopro::miller::is_prime(n)) {
+            return n;
         }
-    }
-    exit(0);
-}
-template <typename mint> 
-std::vector<u64> rho_fact(u64 n) {
-    if (n < 2) {
-        return {};
-    }
-    if (kyopro::miller::is_prime(n)) {
-        return {n};
-    }
-    std::vector<u64> v;
-    std::vector<u64> st{n};
-    while (st.size()) {
-        u64& m = st.back();
-        if (kyopro::miller::is_prime(m)) {
-            v.emplace_back(m);
-            st.pop_back();
-        } else {
-            u64 d = find_factor<mint>(m);
-            m /= d;
-            st.emplace_back(d);
-        }
-    }
-    return v;
-}
-inline std::vector<u64> factorize(u64 n) {
-    if (n < 2) {
-        return {};
-    }
-    auto v = (n < (1uL << 31) ? rho_fact<dynamic_modint<u32>>(n)
-                              : rho_fact<dynamic_modint<u64>>(n));
-    std::sort(v.begin(), v.end());
-    return v;
-}
 
-inline std::vector<pair<u64, int>> exp_factorize(u64 n) {
-    std::vector<u64> pf = factorize(n);
-    if (pf.empty()) {
-        return {};
-    }
-    vector<pair<u64, int>> res;
-    res.emplace_back(pf.front(), 1);
-    for (int i = 1; i < (int)pf.size(); i++) {
-        if (res.back().first == pf[i]) {
-            res.back().second++;
-        } else {
-            res.emplace_back(pf[i], 1);
+        if (mint::get_mod() != n) {
+            mint::set_mod(n);
         }
+        while (1) {
+            u64 c = rng();
+            const auto f = [&](mint x) -> mint { return x * x + c; };
+            mint x = rng();
+            mint y = f(x);
+            u64 d = 1;
+            while (d == 1) {
+                d = _gcd<long long>(
+                    std::abs((long long)x.val() - (long long)y.val()), n);
+                x = f(x);
+                y = f(f(y));
+            }
+            if (1 < d && d < n) {
+                return d;
+            }
+        }
+        exit(0);
+    }
+    template <typename mint>
+    static std::vector<u64> rho_fact(u64 n) {
+        if (n < 2) {
+            return {};
+        }
+        if (kyopro::miller::is_prime(n)) {
+            return {n};
+        }
+        std::vector<u64> v;
+        std::vector<u64> st{n};
+        while (st.size()) {
+            u64& m = st.back();
+            if (kyopro::miller::is_prime(m)) {
+                v.emplace_back(m);
+                st.pop_back();
+            } else {
+                u64 d = find_factor<mint>(m);
+                m /= d;
+                st.emplace_back(d);
+            }
+        }
+        return v;
     }
 
-    return res;
-}
+public:
+    static inline std::vector<u64> factorize(u64 n) {
+        if (n < 2) {
+            return {};
+        }
+        auto v = (n < (1uL << 31) ? rho_fact<dynamic_modint<u32>>(n)
+                                  : rho_fact<dynamic_modint<u64>>(n));
+        std::sort(v.begin(), v.end());
+        return v;
+    }
+
+    static inline std::vector<std::pair<u64, int>> exp_factorize(u64 n) {
+        std::vector<u64> pf = factorize(n);
+        if (pf.empty()) {
+            return {};
+        }
+        std::vector<std::pair<u64, int>> res;
+        res.emplace_back(pf.front(), 1);
+        for (int i = 1; i < (int)pf.size(); i++) {
+            if (res.back().first == pf[i]) {
+                res.back().second++;
+            } else {
+                res.emplace_back(pf[i], 1);
+            }
+        }
+
+        return res;
+    }
 };  // namespace rho
 };  // namespace kyopro
