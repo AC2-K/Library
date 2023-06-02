0e76da199b43a13cd0b6b8b96df0b7de479b70c8
AC2-K <yokouchiko@gmail.com>
2023-06-02 22:21:25 +0900

    [Add]Created "putstr" and "readstr"

--- src/stream.hpp
+++ src/stream.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include <ctype.h>
 #include <stdio.h>
+#include <string>
 namespace kyopro {
+
+/**
+ *  整数の入出力
+ */
 template <typename T> constexpr inline void readint(T& a) {
     a = 0;
     bool is_negative = false;
@@ -20,6 +25,22 @@ template <typename Head, typename... Tail>
 constexpr inline void readint(Head& head, Tail&... tail) {
     readint(head);
     readint(tail...);
+}
+
+template <typename T> void write_int(T a) {
+    if (!a) {
+        putchar_unlocked('0');
+        putchar_unlocked('\n');
+        return;
+    }
+    if (a < 0) putchar_unlocked('-'), a *= -1;
+    char s[37];
+    int now = 37;
+    while (a) {
+        s[--now] = (char)'0' + a % 10;
+        a /= 10;
+    }
+    while (now < 37) putchar_unlocked(s[now++]);
 }
 template <typename T> constexpr inline void putint(T a) {
     if (!a) {
@@ -43,8 +64,27 @@ constexpr inline void putint(Head head, Tail... tail) {
     putint(tail...);
 }
 
+/**
+ * 文字列の入出力
+ */
+
+void readstr(std::string& str) {
+    char c = getchar_unlocked();
+    while (isspace(c)) c = getchar_unlocked();
+    while (!isspace(c)) {
+        str += c;
+        c = getchar_unlocked();
+    }
+}
+void putstr(const std::string& str) {
+    for (auto c : str) {
+        putchar_unlocked(c);
+    }
+    putchar_unlocked('\n');
+}
+
 };  // namespace kyopro
 
 /**
- * @brief 入出力
+ * @brief fastIO
  */
