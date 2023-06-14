c3f91328fc0a2911fed91f32e72ac07ff6e96929
AC2-K <yokouchiko@gmail.com>
2023-06-14 22:07:35 +0900

    [upate]template

--- src/template.hpp
+++ src/template.hpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 #define rep(i, N) for (int i = 0; i < (N); i++)
 #define all(x) (x).begin(), (x).end()
-#define popcount(x) __popcount(x)
+#define popcount(x) __builtin_popcountll(x)
 using i128 = __int128_t;
 using ll = long long;
 using ld = long double;
