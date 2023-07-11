64066575719dbc95925e50861db43f30acbdf746
AC2K <117526989+AC2-K@users.noreply.github.com>
2023-07-11 13:46:23 +0000

    [update]primitive root

--- src/math/primitive_root.hpp
+++ src/math/primitive_root.hpp
@@ -7,31 +7,34 @@ namespace kyopro {
 /**
  * @brief 原始根
  */
-inline uint64_t primitive_root(uint64_t p) {
+template<typename T>
+int primitive_root(T p) {
     if (p == 2) return 1;
+
     auto pf = kyopro::rho::factorize(p - 1);
     pf.erase(std::unique(pf.begin(), pf.end()), pf.end());
     for (auto& q : pf) {
         q = (p - 1) / q;
     }
-    using ull = unsigned long long;
+    
     if (dynamic_modint<uint64_t>::mod() != p) {
         dynamic_modint<uint64_t>::set_mod(p);
     }
-    xor_shift rng(2023);
-    while (1) {
-        dynamic_modint<uint64_t> g(rng());
+
+    for (int g_ = 1; ; ++g_) {
+        dynamic_modint<uint64_t> g(g_);
         if (g.val() == 0) continue;
         bool is_ok = true;
 
         for (auto q : pf) {
-            if (dynamic_modint<uint64_t>(g).pow(q).val() == 1) {
+            if (g.pow(q).val() == 1) {
                 is_ok = false;
                 break;
             }
         }
+
         if (is_ok) {
-            return g.val();
+            return g_;
         }
     }
 }
