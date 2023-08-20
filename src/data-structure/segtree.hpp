c54e46620bf5da0a2fa5b9b854ae4cf0d0272520
AC2K <barney.y2008@gmaiil.com>
2023-08-20 22:13:07 +0900

    [update]segtree

--- src/data-structure/segtree.hpp
+++ src/data-structure/segtree.hpp
@@ -6,7 +6,7 @@ namespace kyopro {
 /**
  * @brief SegmentTree
  */
-template <class S, S (*op)(S, S), S (*e)()> class segtree {
+template <class S, auto op, auto e> class segtree {
     int lg, sz, n;
     std::vector<S> dat;
 
