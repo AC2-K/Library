eb62f4e7a62911e01485816d6986ec8e8d52a4b3
AC2-K <yokouchiko@gmail.com>
2023-09-10 11:04:45 +0900

    [test]test FPS-exp

---
+++ test/yosupo_judge/polynomial/Exp of Formal_Power_Series.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/exp_of_formal_power_series"
+
+#include "../../../src/stream.hpp"
+#include "../../../src/template.hpp"
+#include "../../../src/FormalPowerSeries/FPS.hpp"
+
+using namespace std;
+using namespace kyopro;
+
+using mint = atcoder::modint998244353;
+using fps = FormalPowerSeries<mint>;
+
+int main() {
+    int n;  
+    read(n);
+    fps f(n);
+    rep(i, n) {
+        int v;
+        read(v);
+        f[i] = mint::raw(v);
+    }
+
+    f = f.exp();
+    rep(i, n) put(f[i].val());
+}
