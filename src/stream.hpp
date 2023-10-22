fc147ebc16bf7ea180951c41922834a9b4a47c7a
AC2-K <yokouchiko@gmail.com>
2023-10-22 15:55:05 +0900

    [update]stream

--- src/stream.hpp
+++ src/stream.hpp
@@ -5,13 +5,13 @@
 #include "../src/internal/type_traits.hpp"
 
 namespace kyopro {
-// read
-void single_read(char& c) {
+
+inline void single_read(char& c) {
     c = getchar_unlocked();
     while (isspace(c)) c = getchar_unlocked();
 }
 template <typename T, internal::is_integral_t<T>* = nullptr>
-void single_read(T& a) {
+inline void single_read(T& a) {
     a = 0;
     bool is_negative = false;
     char c = getchar_unlocked();
@@ -26,12 +26,12 @@ void single_read(T& a) {
     if (is_negative) a *= -1;
 }
 template <typename T, internal::is_modint_t<T>* = nullptr>
-void single_read(T& a) {
+inline void single_read(T& a) {
     long long x;
     single_read(x);
     a = T(x);
 }
-void single_read(std::string& str) {
+inline void single_read(std::string& str) noexcept {
     char c = getchar_unlocked();
     while (isspace(c)) c = getchar_unlocked();
     while (!isspace(c)) {
@@ -40,53 +40,53 @@ void single_read(std::string& str) {
     }
 }
 template<typename T>
-void read(T& x) {single_read(x);}
+inline void read(T& x) noexcept {single_read(x);}
 template <typename Head, typename... Tail>
-void read(Head& head, Tail&... tail) {
+inline void read(Head& head, Tail&... tail) noexcept {
     single_read(head), read(tail...);
 }
 
-// write
-void single_write(char c) { putchar_unlocked(c); }
+inline void single_write(char c) noexcept { putchar_unlocked(c); }
 template <typename T, internal::is_integral_t<T>* = nullptr>
-void single_write(T a) {
+inline void single_write(T a) noexcept {
     if (!a) {
         putchar_unlocked('0');
         return;
     }
-    if (a < 0) putchar_unlocked('-'), a *= -1;
-    char s[37];
-    int now = 37;
+    if constexpr (std::is_signed_v<T>) {
+        if (a < 0) putchar_unlocked('-'), a *= -1;
+    }
+    constexpr int d = std::numeric_limits<T>::digits10;
+    char s[d];
+    int now = d;
     while (a) {
         s[--now] = (char)'0' + a % 10;
         a /= 10;
     }
-    while (now < 37) putchar_unlocked(s[now++]);
+    while (now < d) putchar_unlocked(s[now++]);
 }
 template <typename T, internal::is_modint_t<T>* = nullptr>
-void single_write(T a) {
+inline void single_write(T a) noexcept {
     single_write(a.val());
 }
-
-void single_write(const std::string& str) {
+inline void single_write(const std::string& str) noexcept {
     for (auto c : str) {
         putchar_unlocked(c);
     }
 }
-
-template<typename T>
-void write(T x) { single_write(x); }
-template <typename Head, typename... Tail> void write(Head head, Tail... tail) {
+template <typename T> inline void write(T x) noexcept { single_write(x); }
+template <typename Head, typename... Tail>
+inline void write(Head head, Tail... tail) noexcept {
     single_write(head);
     putchar_unlocked(' ');
     write(tail...);
 }
-template <typename... Args> void put(Args... x) {
+template <typename... Args> inline void put(Args... x) noexcept {
     write(x...);
     putchar_unlocked('\n');
 }
 };  // namespace kyopro
 
 /**
- * @brief fastIO
+ * @brief 高速入出力
  */
