f76996af9cacd3e4e0f169fa62bf930086564148
AC2-K <barney.y2008@gmail.com>
2023-12-22 11:50:35 +0900

    [fix]DynamicSegtree

--- src/data-structure/dynamic_segtree.hpp
+++ src/data-structure/dynamic_segtree.hpp
@@ -7,25 +7,26 @@
 
 namespace kyopro {
 template <class S, class Op> class dynamic_segtree {
+    const std::size_t n;
+    const Op op;
+    const S e;
+
     struct Node {
         S val;
         Node *l, *r, *parent;
         
-        constexpr Node(const S& v = e(), Node* pt = nullptr)
+        constexpr Node(const S& v = e, Node* pt = nullptr)
             : val(), l(nullptr), r(nullptr), parent(pt) {}
     };
 
     using uptr = std::unique_ptr<Node>;
     std::vector<uptr> nodes;
     Node* root;
-    Node* make_ptr(const S& v = e(), Node* pt = nullptr) {
+    Node* make_ptr(const S& v = e, Node* pt = nullptr) {
         nodes.emplace_back(std::make_unique<Node>(v, pt));
         return nodes.back().get();
     };
 
-    const std::size_t n;
-    const Op op;
-    const S id;
 
     Node* find(std::size_t i) {
         assert(0 <= i && i < n);
@@ -38,13 +39,13 @@ template <class S, class Op> class dynamic_segtree {
             std::size_t md = (r + l) >> 1;
             if (i < md) {
                 if (!p->l) {
-                    p->l = make_ptr(e(), p);
+                    p->l = make_ptr(e, p);
                 }
                 p = p->l;
                 r = md;
             } else {
                 if (!p->r) {
-                    p->r = make_ptr(e(), p);
+                    p->r = make_ptr(e, p);
                 }
                 p = p->r;
                 l = md;
@@ -54,7 +55,7 @@ template <class S, class Op> class dynamic_segtree {
     }
     void push(Node* p) {
         while (p = p->parent) {
-            p->val = op((p->l ? p->l->val : e()), (p->r ? p->r->val : e()));
+            p->val = op((p->l ? p->l->val : e), (p->r ? p->r->val : e));
         }
     }
 
@@ -83,7 +84,7 @@ public:
     S fold(std::size_t l, std::size_t r) const {
         assert(0 <= l && l <= r && r <= n);
         if (l == r) {
-            return e();
+            return e;
         }
         return internal_fold(root, 0, n, l, r);
     }
@@ -95,7 +96,7 @@ private:
                     std::size_t L,
                     std::size_t R) const {
         if (!p || r <= L || R <= l) {
-            return e();
+            return e;
         }
         if (L <= l && r <= R) {
             return p->val;
