658a6a2aec5c9c69cef0fe62ee51283d7e9f9abd
AC2-K <barney.y2008@gmail.com>
2024-03-22 14:26:52 +0900

    [Add]subset mobius

---
+++ src/algorithm/subset_mobius.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+namespace kyopro {
+/**
+ * @brief subset mobius transform
+ * @param n v : 2^[n] → T, |v| = 2^n
+ */
+template <typename T> void mobius(int n, std::vector<T>& v) {
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < (int)v.size(); ++i) {
+            if (~i >> j & 1) continue;
+            v[i] -= v[i & ~(1 << j)];
+        }
+    }
+}
+};  // namespace kyopro
