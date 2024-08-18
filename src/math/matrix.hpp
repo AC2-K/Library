2f98d5eb9226c69dda88920988935a39e8f055a4
AC2-K <barney.y2008@gmail.com>
2024-08-18 16:33:44 +0900

    [fix]

--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -6,14 +6,16 @@ namespace kyopro {
 
 template <typename T> class Matrix {
     std::vector<std::vector<T>> dat;
-    int h = 0, w = 0;
 
 public:
+    const int h;
+    const int w;
+
     constexpr Matrix(const std::vector<std::vector<T>>& dat)
         : dat(dat), h(dat.size()), w(dat.front().size()) {}
-
+        
     constexpr Matrix(int h_, int w_, T v = T())
-        : dat(h_, std::vector<T>(w_, v)) {}
+        : dat(h_, std::vector<T>(w_, v)), h(h_), w(w_) {}
 
     using mat = Matrix<T>;
     // access
@@ -23,13 +25,15 @@ public:
     constexpr mat& operator+=(const mat& r) {
         assert(r.h == this->h);
         assert(r.w == this->w);
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                dat[i][j] += r.dat[i][j];
+
+        for (int i = 0; i < (this->h); i++) {
+            for (int j = 0; j < (this->w); j++) {
+                this->dat[i][j] += r.dat[i][j];
             }
         }
         return (*this);
     }
+
     constexpr mat& operator-=(const mat& r) {
         assert(r.h == this->h);
         assert(r.w == this->w);
@@ -63,12 +67,17 @@ public:
     constexpr mat operator-(const mat& r) { return mat(*this) -= r; }
     constexpr mat operator*(const mat& r) { return mat(*this) *= r; }
 
+    static mat E(int n) {
+        mat res(n, n, 0);
+        for (int i = 0; i < n; i++) res[i][i] = 1;
+        return res;
+    }
+
     template <typename P> constexpr mat pow(P e) const {
         assert(e >= 0);
         int n = dat.size();
-        mat res(n, n, 0);
+        mat res = mat::E(n);
         mat pr(*this);
-        for (int i = 0; i < n; i++) res[i][i] = 1;
 
         while (e) {
             if (e & 1) res *= pr;
