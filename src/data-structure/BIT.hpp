f46d389b8e2c83fe27783055c7213693dbad9aeb
AC2-K <barney.y2008@gmail.com>
2024-08-25 13:33:14 +0900

    .

--- src/data-structure/BIT.hpp
+++ src/data-structure/BIT.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <cassert>
+
 namespace kyopro {
 
 template <typename T> class BIT {
@@ -9,7 +11,9 @@ template <typename T> class BIT {
 public:
     BIT() : BIT(0) {}
     BIT(int n) : n(n), bit(n + 1, T()) {}
+    
     void add(int p, T w) {
+        assert(0 <= p && p < n);
         p++;
         for (int x = p; x <= n; x += x & -x) {
             bit[x] += w;
@@ -17,6 +21,7 @@ public:
     }
 
     T sum(int p) const {
+        assert(0 <= p && p <= n);
         T s = 0;
 
         for (int x = p; x > 0; x -= x & -x) {
